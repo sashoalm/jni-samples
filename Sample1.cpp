ee6518548e6ecd4ffc8c9eaad2426ef4efe67021
Alexander Almaleh <Alexander.Almaleh@smart-soft.net>
2015-06-05 17:07:14 +0300

    Pass parameter to constructor.

--- Sample1.cpp
+++ Sample1.cpp
@@ -3,8 +3,8 @@
 JNIEXPORT jint JNICALL Java_Sample1_nativeMultiply
   (JNIEnv *env, jclass clazz, jint a, jint b)
 {
-    jmethodID constructor = env->GetMethodID(clazz, "<init>", "()V");
-    jobject object = env->NewObject(clazz, constructor);
+    jmethodID constructor = env->GetMethodID(clazz, "<init>", "(I)V");
+    jobject object = env->NewObject(clazz, constructor, 7);
     jmethodID methodID = env->GetMethodID(clazz, "javaMultiply", "(II)I");
     return env->CallIntMethod(object, methodID, a, b);
 }
--- Sample1.java
+++ Sample1.java
@@ -1,8 +1,13 @@
 class Sample1 {
+    int n;
+    public Sample1(int nn) {
+        n = nn;
+    }
+
     public static native int nativeMultiply(int a, int b);
     public int javaMultiply(int a, int b) {
         System.out.println("javaMultiply(" + a + "," + b + ")=" + (a*b));
-        return a*b;
+        return n*a*b;
     }
     public static void main(String[] args) {
         System.loadLibrary("Sample1");
