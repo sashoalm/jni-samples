a223512df303e85233ccaf8351e79822c8eaa083
Alexander Almaleh <Alexander.Almaleh@smart-soft.net>
2015-06-08 10:15:17 +0300

    Don't accumulate the PATH variable.

--- run.bat
+++ run.bat
@@ -1,4 +1,5 @@
 echo off
+setlocal
 set path=%path%;c:\Program Files\Java\jdk1.8.0_45\bin
 call "c:\Program Files\Microsoft Visual Studio 9.0\VC\vcvarsall.bat"
 cl -I"C:\Program Files\Java\jdk1.8.0_45\include" -I"C:\Program Files\Java\jdk1.8.0_45\include\win32" -LD Sample1.cpp -FeSample1.dll
